b473b758837b188a9c31603cde59f535bb42a25e
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2023-01-01 18:37:43 +0100

    Add vim-polygot (additional filetypes for regex highlighting)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
+  { "sheerun/vim-polyglot", event = "VeryLazy" },
   { "jose-elias-alvarez/typescript.nvim" },
   { "tpope/vim-sleuth", event = "VeryLazy" },
   { "williamboman/mason-lspconfig.nvim" }, -- config in ./mason.lua
